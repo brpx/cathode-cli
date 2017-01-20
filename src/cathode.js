5e5a28f7efc7ed0fe23940787babb041806fa6f9
Celso Martinho <celso@brpx.com>
2017-01-20 18:51:25 +0000

    Auth

--- src/cathode.js
+++ src/cathode.js
@@ -5,6 +5,8 @@ var request = require('superagent');
 var fs = require('fs');
 var chalk = require('chalk');
 
+var data, config;
+
 program
 .arguments('<url>')
 .option('-f, --file <srapfile>', 'use local scrap file')
@@ -13,46 +15,55 @@ program
 .option('-x, --search <string>', 'search string, return probable expression')
 .action(function(url) {
 
+    try {
+        data = fs.readFileSync('~/.cathode');
+        config = JSON.parse(data);
+    }
+    catch (err) {
+        console.log('You need to login to Cathode first.');
+        process.exit(0);
+    }
+
     var scrap = false;
     if(program.file) {
         scrap=fs.readFileSync(program.file).toString();
     }
 
     request
-    .post((process.env.APIURL || 'https://cathode.io') + '/api/scrap')
-    .send({
-        scrap: scrap,
-        url: program.args[0],
-        scope: program.scope,
-        query: program.query,
-        search: program.search
-    })
+        .post((process.env.APIURL || 'https://cathode.io') + '/api/scrap')
+        .send({
+            scrap: scrap,
+            url: program.args[0],
+            scope: program.scope,
+            query: program.query,
+            search: program.search
+        })
     .set('Accept', 'application/json')
-    .end(function (err, res) {
-        if (res && res.ok) {
-            switch(res.body.result) {
-                case 0:
-                    if(program.search) {
-                        for(var i in res.body.items) {
-                            console.log(res.body.items[i]);
-                            console.log("\n");
+        .end(function (err, res) {
+            if (res && res.ok) {
+                switch(res.body.result) {
+                    case 0:
+                        if(program.search) {
+                            for(var i in res.body.items) {
+                                console.log(res.body.items[i]);
+                                console.log("\n");
+                            }
                         }
-                    }
-                    else
-                    {
-                        console.log(res.body.query);
-                    }
-                    break;
-                default:
-                    console.log("Error: "+res.body.result+" "+res.body.error);
-                    break;
+                        else
+                        {
+                            console.log(res.body.query);
+                        }
+                        break;
+                    default:
+                        console.log("Error: "+res.body.result+" "+res.body.error);
+                        break;
+                }
+                process.exit(0);
             }
-            process.exit(0);
-        }
 
-        console.error(chalk.red(err));
-        process.exit(1);
-    });
+            console.error(chalk.red(err));
+            process.exit(1);
+        });
 
 })
 .parse(process.argv);
